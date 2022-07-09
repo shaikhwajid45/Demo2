917fe3a20f7101bf72bed62a58754280873da5f9
shaikhwajid45 <108930211+shaikhwajid45@users.noreply.github.com>
2022-07-09 14:36:45 +0530

    STudent

---
+++ Students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Students
+    {
+
+        public int Studentid { get; set; }
+        public String nm { get; set; }
+        public int age { get; set; }
+        public int Courseid { get; set; }
+
+
+    }
+}
